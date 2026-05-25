a26071c7c9f5ab64d8abdd2a4fbb0b074790e820
Ígor Rueda <igordriguesrueda15@gmail.com>
2026-05-25 17:14:15 +0000

    Finalizando as rotas de plataformas

--- src/routes/plataformas.ts
+++ src/routes/plataformas.ts
@@ -1,4 +1,4 @@
-import { Request,Response,Router } from "express";
+import { Request,response,Response,Router } from "express";
 import { prisma } from '../prisma'
 import router from "./generos";
 
@@ -74,6 +74,18 @@ router.post("/", async (req:Request,res:Response)=>{
                 erro:"Um ou mais jogos não estão cadastrados"
             })
         }
+
+        const novaPlataforma = await prisma.plataforma.create({
+            data:{
+                nome: nome,
+                jogos: idsJogos
+            },
+            include:{
+                jogos: true
+            }
+        })
+
+        res.status(201).json(novaPlataforma)
     }
     
     catch{
@@ -82,6 +94,82 @@ router.post("/", async (req:Request,res:Response)=>{
 
 })
 
+router.put("/:id", async(req:Request, res:Response)=>{
+    try{
+        const id = Number(req.params.id)
 
+        const {nome} = req.body
+
+        if (Number.isNaN(id)){
+            return res.status(400).json({
+                erro:"O ID passado deve ser um número"
+            })
+        }
+
+        if (!nome || nome.trim() === ""){
+            return res.status(400).json({
+                erro:"O nome é um parâmetro obrigatório"
+            })
+        }
+
+        const plataforma = await prisma.plataforma.findUnique({
+            where:{id}
+        })
+
+        if(!plataforma){
+            return res.status(404).json({
+                erro:"A plataforma não foi encointrada"
+            })
+        }
+
+        const plataformaAtualizada = await prisma.plataforma.update({
+            where:{id},
+            data:{nome},
+            include:{jogos: true}
+        })
+
+        res.status(200).json(plataformaAtualizada)
+
+    }
+
+    catch{
+        return res.status(500).json("Erro ao alterar plataforma")
+    }   
+
+})
+
+router.delete("/:id", async (req:Request,res:Response)=>{
+        
+    try{
+
+        const id = Number(req.params.id)
+
+        if (Number.isNaN(id)){
+            return res.status(400).json({
+                erro:"O ID passado deve ser um número!"
+            })
+        }
+
+        const plataforma = await prisma.plataforma.findUnique({
+            where:{id}
+        })
+
+        if(!plataforma){
+            return res.status(404).json({
+                erro:"A plataforma não foi encontrada"
+            })
+        }
+
+        await prisma.plataforma.delete({
+            where:{id}
+        })
+
+        res.status(204).send()
+    }
+
+    catch{
+        return res.status(500).json("Erro ao alterar plataforma")
+    }   
+})
 
 export default router
